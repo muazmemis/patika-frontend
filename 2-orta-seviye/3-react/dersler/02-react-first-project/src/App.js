c6191fa15bc437d0170f48acae5f36f53eff45d5
Muaz Memiş <muazmemis@gmail.com>
2023-03-04 12:06:10 +0300

    Componentlerde Değişken Render Etmek

--- 2-orta-seviye/3-react/dersler/02-react-first-project/src/App.js
+++ 2-orta-seviye/3-react/dersler/02-react-first-project/src/App.js
@@ -1,24 +1,31 @@
 import "./App.css";
 import Header from "./components/Header";
 
+const name = "Muaz";
+const surname = "Memiş";
+
 function App() {
   return (
-    // <div> => <></> or <React.fragment></React.fragment>
+    // <div> => <></> or <React.fragment></React.fragment> => html e dönüştürülürken div tag i oluşturmaz...
     // class yerine className kullanılıyor..
-    <div className="App">
-      <header className="App-header">
-        Hello from App
-        <Header />
-        <p className="xyz">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry.
-        </p>
-        <label htmlFor="myinput">
-          Name
-          <input id="myinput"></input>
-        </label>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          Hello from App
+          <Header />
+          <p className="xyz">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+          </p>
+          <label htmlFor="myinput">
+            Name
+            <input id="myinput"></input>
+          </label>
+          <h1>{name}</h1>
+          <h1>{`${name} ${surname}`}</h1>
+        </header>
+      </div>
+    </>
   );
 
   // return React.createElement("div", null, "Hello");
