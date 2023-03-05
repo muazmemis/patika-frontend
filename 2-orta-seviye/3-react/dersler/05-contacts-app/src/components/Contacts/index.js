cb880bc32a6d0ada5774f3b5a24a38da9c2b4291
Muaz Memiş <muazmemis@gmail.com>
2023-03-05 10:42:27 +0300

    Ne Geliştireceğiz?

---
+++ 2-orta-seviye/3-react/dersler/05-contacts-app/src/components/Contacts/index.js
@@ -0,0 +1,14 @@
+import Form from "./Form";
+import List from "./List";
+
+function Contacts() {
+  return (
+    <div>
+      Contacts
+      <List />
+      <Form />
+    </div>
+  );
+}
+
+export default Contacts;
