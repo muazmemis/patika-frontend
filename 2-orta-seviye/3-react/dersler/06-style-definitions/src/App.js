b1e8fb81e03eb7772f5731e4d0c905ca6558a2b8
Muaz Memiş <muazmemis@gmail.com>
2023-03-05 21:49:08 +0300

    Stil Tanımı Yapmak

--- 2-orta-seviye/3-react/dersler/06-style-definitions/src/App.js
+++ 2-orta-seviye/3-react/dersler/06-style-definitions/src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <p
+          style={{ color: "red", backgroundColor: "white", paddingTop: "10px" }}
         >
-          Learn React
-        </a>
+          Notice the use of %PUBLIC_URL% in the tags above. It will be replaced
+          with the URL of the `public` folder during the build. Only files
+          inside the `public` folder can be referenced from the HTML. Unlike
+          "/favicon.ico" or "favicon.ico", "%PUBLIC_URL%/favicon.ico" will work
+          correctly both with client-side routing and a non-root public URL.
+          Learn how to configure a non-root public URL by running `npm run
+          build`.
+        </p>
+        <div class="alert alert-primary" role="alert">
+          A simple primary alert—check it out!
+        </div>
       </header>
     </div>
   );
