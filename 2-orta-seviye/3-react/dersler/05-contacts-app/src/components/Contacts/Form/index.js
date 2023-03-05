90d3218a252f1919cc016aa811323602436ffeb4
Muaz Memiş <muazmemis@gmail.com>
2023-03-05 13:25:31 +0300

    Form Component'inin Geliştirilmesi

--- 2-orta-seviye/3-react/dersler/05-contacts-app/src/components/Contacts/Form/index.js
+++ 2-orta-seviye/3-react/dersler/05-contacts-app/src/components/Contacts/Form/index.js
@@ -1,5 +1,53 @@
-function Form() {
-  return <div>Form</div>;
+import { useEffect, useState } from "react";
+
+const initialValue = { fullname: "", phone_number: "" };
+
+function Form({ addContact, contacts }) {
+  const [form, setForm] = useState(initialValue);
+
+  useEffect(() => {
+    setForm(initialValue);
+  }, [contacts]);
+
+  const onChangeInput = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (form.fullname === "" || form.phone_number === "") {
+      return false;
+    }
+
+    addContact([...contacts, form]);
+    // setForm(initialValue);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        <input
+          name="fullname"
+          placeholder="Full Name"
+          value={form.fullname}
+          onChange={onChangeInput}
+        />
+      </div>
+      <div>
+        <input
+          name="phone_number"
+          placeholder="Phone Name"
+          value={form.phone_number}
+          onChange={onChangeInput}
+        />
+      </div>
+      {form.fullname} - {form.phone_number}
+      <div>
+        <button>Add</button>
+      </div>
+    </form>
+  );
 }
 
 export default Form;
--- 2-orta-seviye/3-react/dersler/05-contacts-app/src/components/Contacts/index.js
+++ 2-orta-seviye/3-react/dersler/05-contacts-app/src/components/Contacts/index.js
@@ -1,12 +1,19 @@
+import { useEffect, useState } from "react";
 import Form from "./Form";
 import List from "./List";
 
 function Contacts() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    console.log(contacts);
+  }, [contacts]);
+
   return (
     <div>
       Contacts
       <List />
-      <Form />
+      <Form addContact={setContacts} contacts={contacts} />
     </div>
   );
 }
