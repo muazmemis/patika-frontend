4982138b6a199c0329e603a9d7c2a117cce95170
Muaz Memiş <muazmemis@gmail.com>
2023-03-08 13:38:06 +0300

    Multi Context

--- 2-orta-seviye/3-react/dersler/11-context/src/components/Container.js
+++ 2-orta-seviye/3-react/dersler/11-context/src/components/Container.js
@@ -2,15 +2,18 @@ import React, { useContext } from "react";
 import ThemeContext from "../context/ThemeContext";
 import Button from "./Button";
 import Header from "./Header";
+import Profile from "./Profile";
 
 function Container() {
   const { theme } = useContext(ThemeContext);
-  console.log(theme);
+  //   console.log(theme);
 
   return (
     <div className={`app ${theme === "dark" ? "dark" : "light"}`}>
       <Button />
       <Header />
+
+      <Profile />
     </div>
   );
 }
