298f2b4c0ac4471a662c75af7b2681618193d6d4
Muaz Memiş <muazmemis@gmail.com>
2023-03-08 11:28:37 +0300

    Context Oluşturmak

---
+++ 2-orta-seviye/3-react/dersler/11-context/src/components/Button.js
@@ -0,0 +1,9 @@
+import React, { useContext } from "react";
+import ThemeContext from "../context/ThemeContext";
+
+function Button() {
+  const data = useContext(ThemeContext);
+  return <div>Button - {data}</div>;
+}
+
+export default Button;
