5011d72d78692f574c8f219342844e37dfa296aa
Muaz Memiş <muazmemis@gmail.com>
2023-03-08 10:27:13 +0300

    useCallback

--- 2-orta-seviye/3-react/dersler/10-memoization/src/App.js
+++ 2-orta-seviye/3-react/dersler/10-memoization/src/App.js
@@ -1,25 +1,31 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useCallback } from "react";
 import "./App.css";
 import Header from "./components/Header";
 
 // const data = { name: "Muaz" };
 function App() {
   const [number, setNumber] = useState(0);
-  const [text, setText] = useState(0);
+  const [text, setText] = useState("");
 
-  const data = useMemo(() => {
-    // return { name: "Muaz" };
-    return calculateObject();
-  }, []);
+  // const data = useMemo(() => {
+  //   // return { name: "Muaz" };
+  //   return calculateObject();
+  // }, []);
 
   // const data = calculateObject();
+
+  const increment = useCallback(
+    () => setNumber((prevstate) => prevstate + 1),
+    []
+  );
 
   return (
     <div className="App">
       <header className="App-header">
-        <Header number={number < 5 ? 0 : number} data={data} />
+        {/* <Header number={number < 5 ? 0 : number} data={data} /> */}
+        <Header increment={increment} />
         {number}
-        <button onClick={() => setNumber(number + 1)}>increase</button>
+        {/* <button onClick={() => setNumber(number + 1)}>increase</button> */}
 
         <br />
         <br />
@@ -35,12 +41,12 @@ function App() {
   );
 }
 
-function calculateObject() {
-  console.log("Calculating...");
-  for (let i = 0; i < 1000000000; i++) {}
-  console.log("Calculating completed...");
+// function calculateObject() {
+//   console.log("Calculating...");
+//   for (let i = 0; i < 1000000000; i++) {}
+//   console.log("Calculating completed...");
 
-  return { name: "Muaz" };
-}
+//   return { name: "Muaz" };
+// }
 
 export default App;
--- 2-orta-seviye/3-react/dersler/10-memoization/src/components/Header.js
+++ 2-orta-seviye/3-react/dersler/10-memoization/src/components/Header.js
@@ -1,13 +1,16 @@
 import React from "react";
 
-function Header({ number, data }) {
+// function Header({ number, data }) {
+function Header({ increment }) {
   console.log("Header re-rendered");
   return (
     <div>
-      Header - {number}
+      {/* Header - {number} */}
+      Header
       <br />
       <br />
-      <code>{JSON.stringify(data)}</code>
+      {/* <code>{JSON.stringify(data)}</code> */}
+      <button onClick={increment}>increase</button>
     </div>
   );
 }
