0c15ae0bf0bc03bad5e1bee0fd427dcf6c3e8d24
Muaz Memiş <muazmemis@gmail.com>
2023-03-08 08:09:49 +0300

    Form Validasyonları - 2

--- 2-orta-seviye/3-react/dersler/09-formik/src/components/f2-Validation.js
+++ 2-orta-seviye/3-react/dersler/09-formik/src/components/f2-Validation.js
@@ -3,23 +3,34 @@ import { useFormik } from "formik";
 import validationSchema from "./f2-validations";
 
 function Validation() {
-  const { handleSubmit, handleChange, values } = useFormik({
-    initialValues: {
-      email: "",
-      password: "",
-      passwordConfirm: "",
-    },
-    onSubmit: (values) => console.log(values),
-    // validationSchema: validations,
-    validationSchema,
-  });
+  const { handleSubmit, handleChange, handleBlur, values, errors, touched } =
+    useFormik({
+      initialValues: {
+        email: "",
+        password: "",
+        passwordConfirm: "",
+      },
+      onSubmit: (values) => console.log(values),
+      // validationSchema: validations,
+      validationSchema,
+    });
+
+  //   console.log(errors);
 
   return (
     <div>
       <h1>Sign Up</h1>
       <form onSubmit={handleSubmit}>
         <label>Email</label>
-        <input name="email" onChange={handleChange} value={values.email} />
+        <input
+          name="email"
+          onChange={handleChange}
+          value={values.email}
+          onBlur={handleBlur}
+        />
+        {errors.email && touched.email && (
+          <div className="error">{errors.email}</div>
+        )}
 
         <br />
         <br />
@@ -29,7 +40,11 @@ function Validation() {
           name="password"
           onChange={handleChange}
           value={values.password}
+          onBlur={handleBlur}
         />
+        {errors.password && touched.password && (
+          <div className="error">{errors.password}</div>
+        )}
 
         <br />
         <br />
@@ -39,7 +54,11 @@ function Validation() {
           name="passwordConfirm"
           onChange={handleChange}
           value={values.passwordConfirm}
+          onBlur={handleBlur}
         />
+        {errors.passwordConfirm && touched.passwordConfirm && (
+          <div className="error">{errors.passwordConfirm}</div>
+        )}
 
         <br />
         <br />
--- 2-orta-seviye/3-react/dersler/09-formik/src/components/f2-validations.js
+++ 2-orta-seviye/3-react/dersler/09-formik/src/components/f2-validations.js
@@ -1,11 +1,13 @@
 import { object, string, number, date, ref } from "yup";
 
 const validations = object({
-  email: string().email().required(),
-  password: string().min(5).required(),
+  email: string().email("Geçerli bir email giri").required("Zorunlu alan"),
+  password: string()
+    .min(5, "Parolanız en az 5 karakter olmalıdır.")
+    .required("Zorunlu alan"),
   passwordConfirm: string()
-    .oneOf([ref("password")])
-    .required(),
+    .oneOf([ref("password")], "Parolalar eşleşmiyor")
+    .required("Zorunlu alan"),
   //   name: string().required(),
   //   age: number().required().positive().integer(),
   //   website: string().url().nullable(),
