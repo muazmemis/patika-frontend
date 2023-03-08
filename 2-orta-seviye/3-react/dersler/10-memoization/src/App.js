56167f47607aaa8cee8de4f7edec24851e42a202
Muaz Memiş <muazmemis@gmail.com>
2023-03-08 09:58:33 +0300

    useMemo

--- 2-orta-seviye/3-react/dersler/10-memoization/src/App.js
+++ 2-orta-seviye/3-react/dersler/10-memoization/src/App.js
@@ -1,19 +1,46 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import "./App.css";
 import Header from "./components/Header";
 
+// const data = { name: "Muaz" };
 function App() {
   const [number, setNumber] = useState(0);
+  const [text, setText] = useState(0);
+
+  const data = useMemo(() => {
+    // return { name: "Muaz" };
+    return calculateObject();
+  }, []);
+
+  // const data = calculateObject();
 
   return (
     <div className="App">
       <header className="App-header">
-        <Header number={number < 5 ? 0 : number} />
+        <Header number={number < 5 ? 0 : number} data={data} />
         {number}
         <button onClick={() => setNumber(number + 1)}>increase</button>
+
+        <br />
+        <br />
+
+        <input
+          value={text}
+          onChange={({ target }) => {
+            setText(target.value);
+          }}
+        />
       </header>
     </div>
   );
+}
+
+function calculateObject() {
+  console.log("Calculating...");
+  for (let i = 0; i < 1000000000; i++) {}
+  console.log("Calculating completed...");
+
+  return { name: "Muaz" };
 }
 
 export default App;
--- 2-orta-seviye/3-react/dersler/10-memoization/src/components/Header.js
+++ 2-orta-seviye/3-react/dersler/10-memoization/src/components/Header.js
@@ -1,8 +1,15 @@
 import React from "react";
 
-function Header({ number }) {
+function Header({ number, data }) {
   console.log("Header re-rendered");
-  return <div>Header - {number}</div>;
+  return (
+    <div>
+      Header - {number}
+      <br />
+      <br />
+      <code>{JSON.stringify(data)}</code>
+    </div>
+  );
 }
 
 export default React.memo(Header);
