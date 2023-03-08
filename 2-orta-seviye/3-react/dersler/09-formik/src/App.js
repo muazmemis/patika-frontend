0544bc78bc68f9e89b94a18ad9eac8bd667918d7
Muaz Memiş <muazmemis@gmail.com>
2023-03-08 05:59:12 +0300

    Radio / Checkbox / Dropdown

--- 2-orta-seviye/3-react/dersler/09-formik/src/App.js
+++ 2-orta-seviye/3-react/dersler/09-formik/src/App.js
@@ -5,19 +5,27 @@ function App() {
     <div>
       <h1>Sign Up</h1>
       <Formik
+        // bu değerler başlangıçta verdiğimiz değerler. bunlar olmasada çalışır
         initialValues={{
-          firstName: "",
-          lastName: "",
-          email: "",
+          firstName: "Muaz",
+          lastName: "Memiş",
+          email: "muazmemis@gmail.com",
+          gender: "male",
+          hobies: [],
+          country: "turkey",
         }}
         onSubmit={(values) => console.log(values)}
       >
-        {({ handleSubmit, handleChange }) => (
+        {({ handleSubmit, handleChange, values }) => (
           <form onSubmit={handleSubmit}>
             <label htmlFor="firstName" style={{ marginRight: "10px" }}>
               First Name
             </label>
-            <input name="firstName" onChange={handleChange} />
+            <input
+              name="firstName"
+              onChange={handleChange}
+              value={values.firstName}
+            />
 
             <br />
             <br />
@@ -25,7 +33,12 @@ function App() {
             <label htmlFor="lastName" style={{ marginRight: "10px" }}>
               Last Name
             </label>
-            <input name="lastName" placeholder="Last" onChange={handleChange} />
+            <input
+              name="lastName"
+              placeholder="Last"
+              onChange={handleChange}
+              value={values.lastName}
+            />
 
             <br />
             <br />
@@ -39,12 +52,85 @@ function App() {
               placeholder="jane@acme.com"
               type="email"
               onChange={handleChange}
+              value={values.email}
             />
 
             <br />
             <br />
 
+            <label htmlFor="gender" style={{ marginRight: "10px" }}>
+              Gender
+            </label>
+            <span>male</span>
+            <input
+              name="gender"
+              type="radio"
+              value="male"
+              onChange={handleChange}
+              checked={values.gender === "male"}
+            />
+            <span>female</span>
+            <input
+              name="gender"
+              type="radio"
+              value="female"
+              onChange={handleChange}
+              checked={values.gender === "female"}
+            />
+
+            <br />
+            <br />
+
+            <div>
+              Football
+              <input
+                type="checkbox"
+                name="hobies"
+                value="Football"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              Cinema
+              <input
+                type="checkbox"
+                name="hobies"
+                value="Cinema"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              Photography
+              <input
+                type="checkbox"
+                name="hobies"
+                value="Photography"
+                onChange={handleChange}
+              />
+            </div>
+
+            <br />
+            <br />
+
+            <select
+              name="country"
+              onChange={handleChange}
+              value={values.country}
+            >
+              <option value="england">England</option>
+              <option value="turkey">Turkey</option>
+              <option value="usa">USA</option>
+            </select>
+
+            <br />
+            <br />
+
             <button type="submit">Submit</button>
+
+            <br />
+            <br />
+
+            {JSON.stringify(values)}
           </form>
         )}
 
