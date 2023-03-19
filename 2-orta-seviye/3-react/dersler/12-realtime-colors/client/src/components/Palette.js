bd8b1fd55832e0153b7eb02d9e81a96654a519cc
Muaz MEMİŞ <muazmemis@gmail.com>
2023-03-19 18:44:23 +0300

    Arayüzün Hazırlanması

---
+++ 2-orta-seviye/3-react/dersler/12-realtime-colors/client/src/components/Palette.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Palette() {
+  return (
+    <div className="palette">
+      <input type="color" />
+      <button>Click</button>
+    </div>
+  );
+}
+
+export default Palette;
