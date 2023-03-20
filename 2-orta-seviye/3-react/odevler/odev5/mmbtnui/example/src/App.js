a189124f312f90728fb2bd9d3013b82891cbfeea
Muaz Memiş <muazmemis@gmail.com>
2023-03-21 01:45:18 +0300

    react odev 5 - publishing btn project

--- 2-orta-seviye/3-react/odevler/odev5/mmbtnui/example/src/App.js
+++ 2-orta-seviye/3-react/odevler/odev5/mmbtnui/example/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
-import { ExampleComponent } from 'mmbtnui'
+import { Button } from 'mmbtnui'
 import 'mmbtnui/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+      <Button type='b-primary'>Primary Button</Button>
+      <Button type='b-default'>Default Button</Button>
+      <Button type='b-dashed'>Dashed Button</Button>
+      <br />
+      <Button type='b-text'>Text Button</Button>
+      <Button type='b-link' href='...'>
+        Link Button
+      </Button>
+    </>
+  )
 }
 
 export default App
--- 2-orta-seviye/3-react/odevler/odev5/mmbtnui/src/index.js
+++ 2-orta-seviye/3-react/odevler/odev5/mmbtnui/src/index.js
@@ -1,6 +1,33 @@
 import React from 'react'
 import styles from './styles.module.css'
+import PropTypes from 'prop-types'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const Button = (props) => {
+  const { type, href, children } = props
+
+  const getButton = () => {
+    return (
+      <button className={styles[type]} {...props}>
+        {children}
+      </button>
+    )
+  }
+
+  return (
+    <div className={styles['b-btn']}>
+      {type == 'b-link' ? <a href={href}>{getButton()}</a> : getButton()}
+    </div>
+  )
+}
+
+Button.propTypes = {
+  type: PropTypes.string,
+  href: PropTypes.string,
+  children: PropTypes.element
+}
+
+Button.defaultProps = {
+  type: 'b-default',
+  href: '#',
+  children: 'Button'
 }
