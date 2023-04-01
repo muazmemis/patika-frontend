8acab73ae248a3108b3df82f45e9d211ad5a43a1
Muaz Memiş <muazmemis@gmail.com>
2023-04-01 18:45:37 +0300

    Products Ekranının Hazırlanması

--- 2-orta-seviye/3-react/dersler/18-ecommerce-app/client/src/App.js
+++ 2-orta-seviye/3-react/dersler/18-ecommerce-app/client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Signin from "./pages/Auth/Signin";
 import Signup from "./pages/Auth/Signup";
+import Products from "./pages/Products";
 
 function App() {
   return (
@@ -14,17 +15,13 @@ function App() {
 
       <div className="content">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Products />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
       </div>
     </Router>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
 }
 
 export default App;
