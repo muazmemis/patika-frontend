88ce63923afe39909a5ed85be0c4649d7083fd54
Muaz Memiş <muazmemis@gmail.com>
2023-04-01 19:24:43 +0300

    React Query

--- 2-orta-seviye/3-react/dersler/18-ecommerce-app/client/src/App.js
+++ 2-orta-seviye/3-react/dersler/18-ecommerce-app/client/src/App.js
@@ -1,6 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Signin from "./pages/Auth/Signin";
 import Signup from "./pages/Auth/Signup";
--- 2-orta-seviye/3-react/dersler/18-ecommerce-app/client/src/index.js
+++ 2-orta-seviye/3-react/dersler/18-ecommerce-app/client/src/index.js
@@ -4,15 +4,18 @@ import "./index.css";
 import "./reset.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </React.StrictMode>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
