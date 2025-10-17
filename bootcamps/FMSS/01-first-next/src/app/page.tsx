78eda62986f081c803c5700962900db617627b03
Muaz MEMİŞ <muazmemis@gmail.com>
2025-10-17 22:35:51 +0300

    268 FMSS Bilişim Front end Practicum 02.04.23

--- bootcamps/FMSS/01-first-next/src/app/page.tsx
+++ bootcamps/FMSS/01-first-next/src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -66,5 +66,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  );
+  )
 }
