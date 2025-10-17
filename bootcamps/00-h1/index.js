e09c8328e4d9c835bf687136eb4083c3dd649500
Muaz MEMİŞ <muazmemis@gmail.com>
2025-10-17 07:18:01 +0300

    268 FMSS Bilişim Front end Practicum 26.03.23

--- bootcamps/00-h1/index.js
+++ bootcamps/00-h1/index.js
@@ -1,7 +1,29 @@
 // const hi = require('./my-module').default;
 // const { hello: hello2 } = require('./my-module');
-import hi, { hello as hello2 } from './my-module.js';
 
+// hi();
+// hello2();
 
-hi();
-hello2();
+// console.log(slugify('some string'));
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//   .then((response) => response.json())
+//   .then((todo) => console.log('todo:', todo));
+
+// fetch('https://jsonplaceholder.typicode.com/users/1')
+//   .then((response) => response.json())
+//   .then((user) => console.log('User loaded:', user));
+
+// async function fetchData() {
+//   await fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then((res) => res.json())
+//     .then((todo) => console.log('todo:', todo));
+// }
+
+// fetchData();
+
+(async () => {
+  await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then((res) => res.json())
+    .then((todo) => console.log('todo:', todo));
+})();
